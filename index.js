fb387a7586789c3874f8be59d1ab18fc64887688
Guadalupe Martinez <gmartinezbarreira@gmail.com>
2022-10-26 08:31:46 -0300

    proyecto

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function cargarEventListeners() {
 
     // Muestra los productos de Local Storage
     document.addEventListener('DOMContentLoaded', () => {
-        productos = JSON.parse( localStorage.getItem('carroC') ) || [];
+        productos = JSON.parse(localStorage.getItem('carroC')) || [];
 
-         carritoHTML();
+        carritoHTML();
     })
 
 
@@ -25,42 +25,42 @@ function cargarEventListeners() {
     botonVaciar.addEventListener('click', () => {
         productos = [];
 
-         limpiarHTML(); // Eliminamos todo el HTML
+        limpiarHTML(); // Eliminamos todo el HTML
     })
 }
 
 
 // Funciones
-function agregarProducto (e) {
+function agregarProducto(e) {
     e.preventDefault();
 
 
-    if (e.target.classList.contains('agregaralCarro') ) {
+    if (e.target.classList.contains('agregaralCarro')) {
         const artiSeleccionado = e.target.parentElement.parentElement;
-        this.datosArticuloLeeo(artiSeleccionado);
+        datosArticuloLeeo(artiSeleccionado);
     }
 }
-    // Toastify({
+// Toastify({
 
-    //     text: "El Producto agregado correctamente",
-        
-    //     duration: 3000
-    //     // gravedad : "superior izquierda" ,  
-        
-    //    } , 
-    //    onClick : function ( ) { }  
-    //   ) . mostrarbrindis ( ) ;
-        
+//     text: "El Producto agregado correctamente",
+
+//     duration: 3000
+//     // gravedad : "superior izquierda" ,  
+
+//    } , 
+//    onClick : function ( ) { }  
+//   ) . mostrarbrindis ( ) ;
+
 
 
 // Elimina prodcuto del carro de compra
- function eliminarProducto(e) {
-     if (e.target.classList.contains('producto-borrar')) {
-         const articulosId = e.target.getAttribute('data-id');
+function eliminarProducto(e) {
+    if (e.target.classList.contains('producto-borrar')) {
+        const articulosId = e.target.getAttribute('data-id');
 
 
-//         // Saco del arreglo de articulosId por el data-id
-       productos = productos.filter(articulo => articulo.id !== articulosId);
+        //         // Saco del arreglo de articulosId por el data-id
+        productos = productos.filter(articulo => articulo.id !== articulosId);
 
         carritoHTML(); // Iterar sobre el carrito 
     }
@@ -70,19 +70,19 @@ function agregarProducto (e) {
         showCancelButton: true,
         confirmButtonText: 'Borrar',
         denyButtonText: `No borrar`,
-      }).then((result) => {
-       
+    }).then((result) => {
+
         if (result.isConfirmed) {
-          Swal.fire('Borrado!', '', 'success')
+            Swal.fire('Borrado!', '', 'success')
         } else if (result.isDenied) {
-          Swal.fire('Cambios no guardados', '', 'info')
+            Swal.fire('Cambios no guardados', '', 'info')
         }
-      })
+    })
 }
 
 //==========PRUBO ESTA FORMA===== 
 //   function eliminarProducto (e)  {
-    
+
 //     e.preventDefault();
 //     let articulo, articulosId;
 //     if (e.target.classList.contains('producto-borrar')) {
@@ -97,103 +97,107 @@ function agregarProducto (e) {
 function guardarProductosLS(articulo) {
     productos = this.obtenerArtiLocalStorage();
     productos.push(articulo);
-    localStorage.setItem('productos',JSON.stringify(productos));
+    localStorage.setItem('productos', JSON.stringify(productos));
 
 }
 
-function obtenerArtiLocalStorage ( ){
-    let articulosLocalS;
-    if (localStorage.getItem('articulo') === null ){
-      articulosLocalS = [];
+function obtenerArtiLocalStorage() {
+    // let articulosLocalS;
+    if (localStorage.getItem('articulo') === null) {
+        articulosLocalS = [];
     }
-//     else{ articulosLocalS = JSON.parse(localStorage.getItem('articulo'));
-// } return articulosLocalS;
-else{ articulosLocalS = JSON.parse(localStorage.getItem('productos'));
-// } return articulosLocalS ;
+    //     else{ articulosLocalS = JSON.parse(localStorage.getItem('articulo'));
+    // } return articulosLocalS;
+    else {
+        articulosLocalS = JSON.parse(localStorage.getItem('productos'));
+        // } return articulosLocalS ;
 
 
-}
-
-// Lee el contenido del HTML al que le dimos click 
- function datosArticuloLeeo(articulo) { 
-//     // console.log(articulo);
-
-    // Crear un objeto con el contenido del articulo actual
-    const productosInfo = {
-        imagen: articulo.querySelector('img').src,
-        titulo: articulo.querySelector('h4').textContent,
-        precio: articulo.querySelector('.precio').textContent,
-        id: articulo.querySelector('a').getAttribute('data-id'),
-        cantidad: 1,
     }
 
-    let productos;
-    productos = this.obtenerArtiLocalStorage();
-    productos.forEach(function (productos) {
-        if (productos.id === productosInfo) {
-            productos = productos.id;
+    // Lee el contenido del HTML al que le dimos click 
+    function datosArticuloLeeo(articulo) {
+        console.log(articulo);
+
+        // Crear un objeto con el contenido del articulo actual
+        const productosInfo = {
+            imagen: articulo.querySelector('img').src,
+            titulo: articulo.querySelector('h4').textContent,
+            precio: articulo.querySelector('.precio').textContent,
+            id: articulo.querySelector('a').getAttribute('data-id'),
+            cantidad: 1,
         }
-    });
 
-}
+        let productos;
+        productos = this.obtenerArtiLocalStorage();
+        productos.forEach(function (productos) {
+            if (productos.id === productosInfo) {
+                productos = productos.id;
+            }
+        });
+
+    }
 
 
-// me fijo si un producto ya lo habia tomado en el carro
-const existeProduc = productos.some(articulo => articulo.id === productos.id);
-if (existeProduc) {
-    // Actualizamos la cantidad
-    const articulo = productos.map(articulo => {
-        if (articulo.id === productos.id) {
-            articulo.cantidad++;
-            return articulo;
-        } else {
-            return articulo; // retorna los objetos que no son los duplicados
+    // me fijo si un producto ya lo habia tomado en el carro
+    const existeProduc = productos.some(articulo => articulo.id === productosInfo.id);
+    if (existeProduc) {
+        // Actualizamos la cantidad
+        const articulo = productos.map(articulo => {
+            if (articulo.id === productosInfo) {
+                articulo.cantidad++;
+                return articulo;
+            } else {
+                return articulo; // retorna los objetos que no son los duplicados
+            }
+        });
+        productos = [...articulo];
+    } else {
+        // Agrega elementos al arreglo de carrito
+        productos = [...productos,productosInfo ];
+    }
+    console.log(productos);
+
+    carritoHTML();
+
+    //Calculo precios
+    totalCalcular(); {
+        let articulosLocalS;
+        let total = 0;
+        articulosLocalS = this.obtenerArtiLocalStorage();
+        for (let i = 0; i < articulosLocalS.length; i++) {
+            let elementos = Number(articulosLocalS[i].precio * articulosLocalS[i].cantidad);
+            total = total + elementos;
         }
-    });
-    productos = [...articulo];
-} else {
-    // Agrega elementos al arreglo de carrito
-    productos = [...productos, productos];
-}
-console.log(productos);
-
-carritoHTML();
-
-//Calculo precios
-totalCalcular(); {
-    let articulosLocalS;
-    let total = 0;
-    articulosLocalS = this.obtenerArtiLocalStorage();
-    for (let i = 0; i < articulosLocalS.length; i++) {
-        let elementos = Number(articulosLocalS[i].precio * articulosLocalS[i].cantidad);
-        total = total + elementos;
+        document.getElementById('total').carritoHTML = "S/." + total.toFixed(2);
     }
-    document.getElementById('total').carritoHTML = "S/." + total.toFixed(2);
-}
-eventoO(e); {
-    e.preventDefault();
-    let id, cantidad, productos, articulosLocalS;
-    if (e.target.classList.contains('cantidad')) {
-        articulo = e.target.parentElement.parentElement;
-        id = articulo.querySelector('a').getAttribute('data-id');
-        cantidad = articulo.querySelector('input').value;
-         articulosLocalS = this.obtenerArtiLocalStorage();
+    eventoO(e); {
+        e.preventDefault();
+        let id, cantidad, productos, articulosLocalS;
+        if (e.target.classList.contains('cantidad')) {
+            articulo = e.target.parentElement.parentElement;
+            id = articulo.querySelector('a').getAttribute('data-id');
+            cantidad = articulo.querySelector('input').value;
+            articulosLocalS = this.obtenerArtiLocalStorage();
 
 
+        }
     }
-}
 
 
 
-// Muestra el Carrito de compras en el HTML
-function carritoHTML() {
-    limpiarHTML();
+    // Muestra el Carrito de compras en el HTML
+    // const contendio = element.carritoHTML;
+    // element.carritoHTML = htmlString;
 
-    // Recorre el carrito y genera el HTML
-    productos.forEach(articulo => {
-        const { imagen, titulo, precio, cantidad, id } = articulo;
-        const row = document.createElement('tr');
-        row.carritoHTML = `
+    function carritoHTML() {
+        limpiarHTML();
+
+        // Recorre el carrito y genera el HTML
+        productos.forEach(articulo => {
+            const { imagen, titulo, precio, cantidad, id } = articulo;
+            const row = document.createElement('tr');
+            row.carritoHTML = `
             <td> <img src="${imagen}" width="100"></td>
             <td>${titulo}</td>
             <td>${precio}</td>
@@ -204,23 +208,25 @@ function carritoHTML() {
             </td>
         `;
 
-        // Agrega el HTML del carrito en el tbody
-        contenedorCarrito.appendChild(row);
-    });
+            // Agrega el HTML del carrito en el tbody
+            contenedorCarrito.appendChild(row);
+        });
 
-    // Agregar el carrito de compras al storage
-    sincronizarStorage();
+        // Agregar el carrito de compras al storage
+        sincronizarStorage();
 
-}
-
-function sincronizarStorage() {
-    localStorage.setItem('carroC', JSON.stringify(productos));
-}
-
-// Elimina los cursos del tbody
-function limpiarHTML() {
-
-    while (contenedorCarrito.firstChild) {
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
-}}
+
+    function sincronizarStorage() {
+        localStorage.setItem('carrito', JSON.stringify(productos));
+    }
+
+    // Elimina los cursos del tbody
+    function limpiarHTML() {
+
+        while (contenedorCarrito.firstChild) {
+            contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+        }
+    }
+}
+
